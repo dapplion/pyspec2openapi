4b14eb591dca96a3f2bfe478abe2858c8f90cc4d
dapplion <35266934+dapplion@users.noreply.github.com>
2024-01-06 18:31:58 +0700

    v0.2.0

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages  # type: ignore
-
-setup(
-    name='pyspec2openapi',
-    version='0.1.0',
-    packages=find_packages(),
-    install_requires=[],  # Dependencies listed in requirements.txt
-)
